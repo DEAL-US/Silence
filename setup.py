b86a7bd63b7147d457c81cc9dbb9696fac0f5c9c
Agustín Borrego <borrego@us.es>
2020-05-24 12:16:30 +0200

    Fixed a wrong link in setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description='An educational API+Web framework.',
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url='http://github.com/IISSI-US/Silence',
+    url='http://github.com/agu-borrego/Silence',
     author='Agustín Borrego',
     author_email='borrego@us.es',
     license='MIT',
