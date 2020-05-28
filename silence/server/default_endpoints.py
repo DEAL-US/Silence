6cb0840c49a98e94e1c985bdf7eb75d1f7c1589c
Agustín Borrego <borrego@us.es>
2020-05-28 12:08:54 +0200

    Implemented the /register endpoint

--- silence/server/default_endpoints.py
+++ silence/server/default_endpoints.py
@@ -3,7 +3,7 @@ from werkzeug.security import check_password_hash, generate_password_hash
 
 from silence.auth.tokens import create_token, check_token
 from silence.db import dal
-from silence.sql.builder import get_login_query
+from silence.sql.builder import get_login_query, get_register_user_query
 from silence.settings import settings
 from silence.exceptions import HTTPError
 
@@ -51,4 +51,36 @@ def login():
     return jsonify(res), 200
 
 def register():
-    pass
+    # Ensure that the user has sent the required fields
+    username = request.form.get(IDENTIFIER_FIELD, default=None)
+    password = request.form.get(PASSWORD_FIELD, default=None)
+
+    # Ensure that the identifier is unique
+    login_q = get_login_query(USERS_TABLE, IDENTIFIER_FIELD, username)
+    other_users = dal.api_safe_query(login_q)
+
+    if other_users:
+        raise HTTPError(400, f"There already exists another user with that {IDENTIFIER_FIELD}")
+
+    # Create the user object, replacing the password with the hashed one
+    user = dict(request.form)
+    user[PASSWORD_FIELD] = generate_password_hash(password)
+
+    # Try to insert it in the DB
+    # Since the /register endpoint must adapt to any possible table,
+    # we assume that the user knows what they're doing and submits the
+    # appropriate fields. Otherwise, the DB will just complain.
+    register_q = get_register_user_query(USERS_TABLE, user)
+    dal.api_safe_update(register_q)
+
+    # Fetch the newly created user from the DB (some fields may have been)
+    # automatically generated, like its ID
+    # It should now exist
+    user = dal.api_safe_query(login_q)[0]
+
+    # If we've reached here the register is successful, generate a session token
+    # and return it with the logged user's info
+    token = create_token(user)
+    del user[PASSWORD_FIELD]
+    res = {"sessionToken": token, "user": user}
+    return jsonify(res), 200
